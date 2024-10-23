dcea3724ad7d4fbabc55efbf4a3cb941baa8cb16
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-10-23 12:24:25 +0200

    Actualizaciones varias - Añadida metaetiqueta de keywords. - Añadida animación de parpadeo usando keyframes. - Añadida funcionalidad de overlay para forzar click y asegurar la reproducción del audio de fondo. - Modificada la funcionalidad de cambio de imagen para usar variables globales con el getElementById y que el código sea un poco más simple.

--- js/effects.js
+++ js/effects.js
@@ -1,19 +1,20 @@
-// Efecto de sonido aterrador al cargar la página
-window.onload = function() {    
-    var audio = new Audio('/sounds/horror-sounds.mp3');  // Crear un nuevo objeto de audio
-    audio.volume = 0.3;  // Volumen al 30% para dar ambiente sin ser molesto
-    audio.loop = true;   // Sonido en bucle para mantener el ambiente
-    audio.play();   // Inicia la reproducción automáticamente cuando la página haya cargado
-};
+const overlay = document.getElementById('overlay');  // Overlay de la página para mostrar al inicio
+const imagenPrincipal = document.getElementById('imagen-principal');  // Imagen principal de la página
+const risaAudio = document.getElementById('risa-audio');  // Audio de risa maléfica
+const ambienteAudio = document.getElementById('ambiente-audio');  // Audio de ambiente tenebroso
+ambienteAudio.volume = 0.3;  // Volumen al 30% para dar ambiente sin ser molesto
 
-// Crear el audio de la risa maléfica
-var risaAudio = new Audio('sounds/risa-malefica.mp3');
+// Función para iniciar el audio de ambiente cuando el usuario hace clic en la página
+function iniciar() {
+    overlay.style.display = 'none';  // Ocultar el overlay
+    ambienteAudio.play();  // Empezar a reproducir el audio de ambiente
+}
 
 // Función para cambiar la imagen a una aterradora y reproducir la risa maléfica
 function cambiarImagen() {
     
     // Cambia la imagen principal a una imagen aterradora
-    document.getElementById('imagen-principal').src = 'images/scary_image.jpg';
+    imagenPrincipal.src = 'images/scary_image.jpg';
 
     // Empezar a reproducir el audio
     risaAudio.play();
@@ -22,7 +23,7 @@ function cambiarImagen() {
 // Función para restaurar la imagen original y detener la risa maléfica
 function restaurarImagen() {
     // Cambia la imagen principal de vuelta a la imagen original
-    document.getElementById('imagen-principal').src = 'images/sombra-tenebrosa.jpg';    
+    imagenPrincipal.src = 'images/sombra-tenebrosa.jpg';    
 
     // Detener el audio (pausar la risa maléfica)
     risaAudio.pause();
@@ -30,4 +31,3 @@ function restaurarImagen() {
     // Reiniciar el audio al inicio para que la próxima vez empiece desde el principio
     risaAudio.currentTime = 0; 
 }
-
